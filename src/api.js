07ff4ddcc60fc20c799fd86a79f04f93ded93c11
elyasamiri <100440198+elyasamiri@users.noreply.github.com>
2023-10-15 05:28:36 +0430

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "dd4e6b6a4cmsh7b7f189933011e6p12a4a4jsnb6d03802721c",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "a31470f9974c7927ba286019e0048d47";
